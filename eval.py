79710c48b25de6889a519148a973e2309bc76417
mazm17 <joeminmar@yeah.net>
2018-06-16 17:06:20 +0800

    In case of: out of memory error

--- eval.py
+++ eval.py
@@ -12,6 +12,18 @@ import opts
 from model import Ctr
 from dataset import UserTest
 
+
+def results_to_file(luser_id, lphoto_id, lpred, le_user):
+    luser_id = torch.cat(luser_id, dim=0).cpu().numpy()
+    lphoto_id = torch.cat(lphoto_id, dim=0).cpu().numpy()
+    lpred = torch.cat(lpred, dim=0).squeeze(1).cpu().numpy()
+
+    luser_id = le_user.inverse_transform(luser_id)
+    with open(os.path.join('results', 'submit.txt'), 'a') as f:
+        for user_id, photo_id, pred in zip(luser_id, lphoto_id, lpred):
+            f.write('\t'.join([str(user_id), str(photo_id), '%.6f' % pred]) + '\n')
+
+
 if __name__ == "__main__":
     opt = opts.parse_opt()
     with open(os.path.join('models', 'le_user.param'), 'rb') as f:
@@ -23,7 +35,11 @@ if __name__ == "__main__":
 
     model = Ctr(opt=opt).eval()
     model = model.cuda()
-    model.load_state_dict(torch.load(os.path.join('models', 'model-3.ckpt')))
+    model.load_state_dict(torch.load(os.path.join('models', 'model-4.ckpt')))
+
+    # clean the submit file
+    with open(os.path.join('results', 'submit.txt'), 'w') as f:
+        f.write('')
 
     luser_id, lphoto_id, lpred = [], [], []
     for i, data in enumerate(tqdm(test_dataloader)):
@@ -32,17 +48,13 @@ if __name__ == "__main__":
         pred = model(user_id, visual_feature)
         pred = F.sigmoid(pred)
 
-        luser_id.append(user_id)
-        lphoto_id.append(photo_id)
-        lpred.append(pred)
+        luser_id.append(user_id.detach())
+        lphoto_id.append(photo_id.detach())
+        lpred.append(pred.detach())
 
-    print("Evaluation finished. Now save it into {} ...".format(os.path.join('results', 'submit.txt')))
-    luser_id = torch.cat(luser_id, dim=0).detach().cpu().numpy()
-    lphoto_id = torch.cat(lphoto_id, dim=0).detach().cpu().numpy()
-    lpred = torch.cat(lpred, dim=0).squeeze(1).detach().cpu().numpy()
+        # In case of out of memory error
+        if (i + 1) % 500 == 0:
+            results_to_file(luser_id, lphoto_id, lpred, le_user)
+            luser_id, lphoto_id, lpred = [], [], []
 
-    luser_id = le_user.inverse_transform(luser_id)
-
-    with open(os.path.join('results', 'submit.txt'), 'w') as f:
-        for user_id, photo_id, pred in zip(tqdm(luser_id), lphoto_id, lpred):
-            f.write('\t'.join([str(user_id), str(photo_id), '%.6f' % pred]) + '\n')
+    results_to_file(luser_id, lphoto_id, lpred, le_user)
